039efba575cd66d2a035380d5e93c6d09d0e3204
muhammadasad928 <muhammadasad2847@gmail.com>
2024-11-28 16:29:55 +0500

    Utils code practice

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import express from 'express'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`App is listning at port: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("DB connection failed !!! ", err)
+})
