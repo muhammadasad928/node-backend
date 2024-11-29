a7b1087b8d8b9e525a87d5277c0c475f34c02ce3
muhammadasad928 <muhammadasad2847@gmail.com>
2024-11-29 23:05:56 +0500

    router and controller settings

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({extended:true, limit:"20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+//routes
+import userRouter from './routes/user.routes.js'
+
+app.use('/api/users', userRouter)
 export { app };
